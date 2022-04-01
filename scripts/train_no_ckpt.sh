d885e9a9d1a2b36722b240b84261589995570c7d
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-04-02 08:19:18 +0900

    aa

--- scripts/train_no_ckpt.sh
+++ scripts/train_no_ckpt.sh
@@ -9,11 +9,11 @@ then
     echo "Expect two args to specify config file and suffix"
 elif [[ $# -eq 2 ]]
 then
-    python -u habitat_baselines/run.py --run-type train --exp-config habitat_baselines/config/objectnav/$1.on.yaml --run-suffix $2
+    python -u -m habitat_baselines/run.py --run-type train --exp-config habitat_baselines/config/objectnav/$1.on.yaml --run-suffix $2
 elif [[ $# -ge 3 ]]
 then
     all_args=("$@")
     rest_args=("${all_args[@]:2}")
     echo ${rest_args[@]}
-    python -u habitat_baselines/run.py --run-type train --exp-config habitat_baselines/config/objectnav/$1.on.yaml --run-suffix $2 ${rest_args[@]}
+    python -u -m habitat_baselines/run.py --run-type train --exp-config habitat_baselines/config/objectnav/$1.on.yaml --run-suffix $2 ${rest_args[@]}
 fi
